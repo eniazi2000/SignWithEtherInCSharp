fecac7f5e91bdb12de21bc747ff23cd26213df78
eniazi2000 <e.niazi.2000@gmail.com>
2022-08-24 11:06:46 +0430

    version  0.1

---
+++ SignWithEtherDemo/SignWithEtherDemo/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace SignWithEtherDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PrivateKeyTxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PublickKeyTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MessageTxt = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SignatureTxt = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PrivateKeyTxt
+            // 
+            this.PrivateKeyTxt.Location = new System.Drawing.Point(345, 55);
+            this.PrivateKeyTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.PrivateKeyTxt.Name = "PrivateKeyTxt";
+            this.PrivateKeyTxt.Size = new System.Drawing.Size(1168, 30);
+            this.PrivateKeyTxt.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1313, 166);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate wallet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Private Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Address(Publick Key)";
+            // 
+            // PublickKeyTxt
+            // 
+            this.PublickKeyTxt.Location = new System.Drawing.Point(345, 112);
+            this.PublickKeyTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.PublickKeyTxt.Name = "PublickKeyTxt";
+            this.PublickKeyTxt.Size = new System.Drawing.Size(1168, 30);
+            this.PublickKeyTxt.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(136, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Message To sign";
+            // 
+            // MessageTxt
+            // 
+            this.MessageTxt.Location = new System.Drawing.Point(345, 230);
+            this.MessageTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.MessageTxt.Name = "MessageTxt";
+            this.MessageTxt.Size = new System.Drawing.Size(1168, 30);
+            this.MessageTxt.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1313, 289);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 35);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sign Message";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(203, 356);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Signature";
+            // 
+            // SignatureTxt
+            // 
+            this.SignatureTxt.Location = new System.Drawing.Point(345, 353);
+            this.SignatureTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.SignatureTxt.Name = "SignatureTxt";
+            this.SignatureTxt.Size = new System.Drawing.Size(1168, 30);
+            this.SignatureTxt.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(692, 455);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 35);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Verify Message";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1625, 1019);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SignatureTxt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MessageTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PublickKeyTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PrivateKeyTxt);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Demo ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PrivateKeyTxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PublickKeyTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox MessageTxt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox SignatureTxt;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SignWithEtherDemo/SignWithEtherDemo/Form1.cs
@@ -0,0 +1,49 @@
+﻿using Nethereum.Signer;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignWithEtherDemo
+{
+    public partial class Form1 : Form
+    {
+        public Account account;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EthECKey key = EthECKey.GenerateKey();
+            byte[] privateKey = key.GetPrivateKeyAsBytes();
+            account = new Account(privateKey);
+            PrivateKeyTxt.Text = account.PrivateKey;
+            PublickKeyTxt.Text = account.Address;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var signer = new EthereumMessageSigner();
+            //var signature = signer.EncodeUTF8AndSign(MessageTxt.Text, new EthECKey(account.PrivateKey));
+            var signature = signer.HashAndSign(MessageTxt.Text, account.PrivateKey);
+            SignatureTxt.Text = signature;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var signer = new EthereumMessageSigner();
+            //var res = signer.EncodeUTF8AndEcRecover(MessageTxt.Text, SignatureTxt.Text);
+            var res = signer.HashAndEcRecover(MessageTxt.Text, SignatureTxt.Text);
+            MessageBox.Show(res);
+        }
+    }
+}
